76f9199da1462a4d34a7c6df0e7af2fc1ce175e0
Tsereh <tserehbros@gmail.com>
2019-02-18 06:45:34 +0200

    Add snapshot to App

--- src/App.test.js
+++ src/App.test.js
@@ -2,20 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { shallow } from "enzyme";
 import App from './App';
+import renderer from 'react-test-renderer';
 
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
-    ReactDOM.unmountComponentAtNode(div);
-});
+describe("App component", () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<App />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
 
-it("should remove duplicate entries/add new entries", () => {
-    let app = shallow(<App/>);
+    it("should remove duplicate entries/add new entries", () => {
+        let app = shallow(<App/>);
 
-    app.instance().togglePlaceBookmark("Helsinki", false);
-    app.instance().togglePlaceBookmark("Vantaa", false);
-    app.instance().togglePlaceBookmark("Helsinki", true);
-    app.instance().togglePlaceBookmark("Espoo", false);
+        app.instance().togglePlaceBookmark("Helsinki", false);
+        app.instance().togglePlaceBookmark("Vantaa", false);
+        app.instance().togglePlaceBookmark("Helsinki", true);
+        app.instance().togglePlaceBookmark("Espoo", false);
 
-    expect(app.state().savedPlaces).toEqual(["Espoo", "Vantaa"]);
+        expect(app.state().savedPlaces).toEqual(["Espoo", "Vantaa"]);
+    });
+
+    it("matches the snapshot", () => {
+        const tree = renderer.create(<App/>).toJSON();
+
+        expect(tree).toMatchSnapshot();
+    });
 });
